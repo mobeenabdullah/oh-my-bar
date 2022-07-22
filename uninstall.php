a46e8993753fb0dc71256a8b8e7b4d1e23af2dfa
sajjadcodes <sakraan01@gmail.com>
2022-07-22 23:54:49 +0500

    cleanup options settings during uninstall plugin

--- uninstall.php
+++ uninstall.php
@@ -29,3 +29,20 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+$read_bar_settings = [
+	'rb_background_color',
+	'rb_foreground_color',
+	'rb_background_opacity',
+	'rb_bar_shadow',
+	'rb_bar_rounded',
+	'rb_bar_placement',
+	'rb_bar_height',
+	'rb_show_home_page',
+	'rb_show_single_post',
+	'rb_show_single_page',
+	'rb_show_archive',            
+];
+
+foreach ( $read_bar_settings as $settingName ) {
+	delete_option( $settingName );
+}
