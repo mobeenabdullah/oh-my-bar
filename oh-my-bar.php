728f3de0a0e763c8fecc6e988b3a7785fd349f9f
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-29 04:34:58 +0500

    Update oh-my-bar.php

--- oh-my-bar.php
+++ oh-my-bar.php
@@ -22,6 +22,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       oh-my-bar
+ * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
